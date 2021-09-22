694fba1550bbbc62a5ec8e02f1ec32f0cb031b97
kloploid <kloploid@gmail.com>
2021-09-22 11:16:29 +0300

    Rasmus Vare ettepanekul parendatud kood

--- k1/src/ColorSort.java
+++ k1/src/ColorSort.java
@@ -1,40 +1,38 @@
 public class ColorSort {
 
-   enum Color {red, green, blue}
+    enum Color {red, green, blue}
 
-   public static void main (String[] param) {
-      // for debugging
-   }
-   public static void reorder (Color[] balls) {
-      Color[] temp = new Color[balls.length];
-      int red = 0;
-      int blue = 0;
-      if (balls.length == 0 || balls.length == 1) {
-         return;
-      }
-      for (Color item : balls){
-         if (item == Color.red){
-            red += 1;
-         }
-         if (item == Color.blue){
-            blue -= 1;
-         }
-      }
-      int green = balls.length + (blue - red);
-      int redcons = red;
-      while (red + green - blue > 0 ){
-         if(red > 0){
-            balls[red - 1] = Color.red;
-            red -= 1;
-         }
-          if (green > 0){
-            balls[green + redcons - 1] = Color.green;
-            green -= 1;
-         }
-          if (blue < 0){
-                balls[balls.length + blue - 1] = Color.blue;
+    public static void main(String[] param) {
+        // for debugging
+    }
+
+    public static void reorder(Color[] balls) {
+        //Balls counting in array
+        int red = 0;
+        int blue = 0;
+        for (Color ball : balls) {
+            if (ball == Color.red) {
+                red += 1;
+            }
+            if (ball == Color.blue) {
                 blue += 1;
-             }
-         }
-      }
-   }
+            }
+        }
+        int green = balls.length - (red + blue);
+        //Balls insert in array
+        int index = 0;
+        while (index <= balls.length) {
+            if (red > 0) {
+                balls[index] = Color.red;
+                red -= 1;
+            } else if (green > 0) {
+                balls[index] = Color.green;
+                green -= 1;
+            } else if (blue > 0) {
+                balls[index] = Color.blue;
+                blue -= 1;
+            }
+            index += 1;
+        }
+    }
+}
